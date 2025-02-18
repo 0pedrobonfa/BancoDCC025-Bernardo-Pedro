330720823e0f71d41ade6cdf88730493170e5fcd
0pedrobonfa <pedro.bonfa@estudante.ufjf.br>
2025-02-18 17:29:01 -0300

    Testa commit

--- src/main/java/br/ufjf/dcc/dcc205/bancodcc025/BancoDCC025.java
+++ src/main/java/br/ufjf/dcc/dcc205/bancodcc025/BancoDCC025.java
@@ -1,15 +1,10 @@
 
 package br.ufjf.dcc.dcc205.bancodcc025;
 
-/**
- *
- * @author User
- */
 public class BancoDCC025 {
 
     public static void main(String[] args) {
-        System.out.println();
         System.out.println("Hello World!");
-        System.out.println();
+        System.out.println("Testa commit");
     }
 }
