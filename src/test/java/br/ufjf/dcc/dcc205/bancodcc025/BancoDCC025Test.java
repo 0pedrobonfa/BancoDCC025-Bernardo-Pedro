43863942da7ca2a54b3a8644ec39e7ef7e688114
Bressan48 <bernardobressandemattos@gmail.com>
2025-03-04 14:41:39 -0300

    adiciona esqueleto dos testes

---
+++ src/test/java/br/ufjf/dcc/dcc205/bancodcc025/BancoDCC025Test.java
@@ -0,0 +1,79 @@
+
+package br.ufjf.dcc.dcc205.bancodcc025;
+import br.ufjf.dcc.dcc205.bancodcc025.Cliente;
+import br.ufjf.dcc.dcc205.bancodcc025.Caixa;
+import br.ufjf.dcc.dcc205.bancodcc025.TelaLogin;
+import br.ufjf.dcc.dcc205.bancodcc025.Usuario;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BancoDCC025Test {
+    
+    @Test
+    public void testaTransferenciaCliente ()
+    {
+    }
+    
+    @Test
+    public void testaConsultaDeSaldoCliente ()
+    {
+    }
+    
+    @Test
+    public void testaInvestimentoEmRendaFixaCliente ()
+    {
+    }
+    
+    @Test
+    public void testaInvestimentoEmRendaVariavelCliente ()
+    {
+    }
+    
+    @Test
+    public void testaSolicitaçãoDeCreditoCliente ()
+    {
+    }
+    
+    @Test
+    public void testaAtendimentoDeSaqueCaixa ()
+    {
+    }
+    
+    @Test
+    public void testaProcessamentoDeDepositosCaixa ()
+    {
+    }
+    
+    @Test
+    public void testaTransferenciaCaixa ()
+    {
+    }
+    
+    @Test
+    public void testaApoioEmMovimentacoesFinanceirasGerente ()
+    {
+    }
+    
+    @Test
+    public void testaCadastroDeOpcoesDeRendaFixaGerente ()
+    {
+    }
+    
+    @Test
+    public void testaCadastroDeOpcoesDeRendaVariavelGerente ()
+    {
+    }
+    
+    @Test
+    public void testaAvaliacaoDeCreditoGerente ()
+    {
+    }
+}
